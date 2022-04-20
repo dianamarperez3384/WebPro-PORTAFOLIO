6513ae0629976569a31af57977ad088e2af25e36
dianamarperez3384 <dianalizbethmarperez2@gmail.com>
2022-04-20 14:47:17 -0500

    se agrega el mensaje al main

--- app/vistas/portafolio/portafolio.php
+++ app/vistas/portafolio/portafolio.php
@@ -1,8 +1,8 @@
 <div>
-    <li>portafolio</li>
-
-        <p >Diana Lizbeth Mar Perez 3384 
-            Ing. Tecnologias de la informacion
-            soy una persona tranquila, alegre, me gusta escuchar musica en mi tiempo libre<br>
+    <h1 id="titulo-principal" class="titulo1">Nuestros Datos Personales</h1>
+        <p >Diana Lizbeth Mar Perez 3384<br>
+            <p > Ing. Tecnologias de la informacion<br>
+           <p > soy una persona tranquila, alegre, me gusta escuchar musica en mi tiempo libre<br>
+            <p >Tecnico en Electronica  <br>
         </p> 
 </div>
